e6b1d6b0b773312de5f479513594741b0b33a439
sgk <sgk2425@gmail.com>
2023-06-05 08:54:53 +0530

    Revoke chnages

--- src/main/java/com/company/base/BaseClass.java
+++ src/main/java/com/company/base/BaseClass.java
@@ -12,7 +12,7 @@ import org.testng.annotations.BeforeSuite;
 
 public class BaseClass {
 
-	public static final String currDir=System.getProperty("user.dir");
+	public static String currDir=System.getProperty("user.dir");
 	public static WebDriver driver;
 	public static Properties pro;
 
@@ -21,16 +21,14 @@ public class BaseClass {
 		
 		loadConfig(currDir+"\\src\\test\\resources\\config\\config.properties");
 		
-		String browser=pro.getProperty("browser");
-		
-		if ("chrome".equalsIgnoreCase(browser)) {
+		if ("chrome".equalsIgnoreCase(pro.getProperty("browser"))) {
 			driver=new ChromeDriver();
-		}else if ("firefox".equalsIgnoreCase(browser)) {
+		}else if ("firefox".equalsIgnoreCase(pro.getProperty("browser"))) {
 			driver=new FirefoxDriver();
-		}else if ("edge".equalsIgnoreCase(browser)) {
+		}else if ("edge".equalsIgnoreCase(pro.getProperty("browser"))) {
 			driver=new EdgeDriver();
 		}else {
-			System.out.println("Given browser " +browser+" have to impletemnt ......");
+			System.out.println("Given browser " +pro.getProperty("browser")+" have to impletemnt ......");
 		}
 		
 		driver.manage().window().maximize();
